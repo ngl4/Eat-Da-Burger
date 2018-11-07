0cbd84c641ea32e896ab4e5098cb4e91abbdeb77
Binh Lam <cindybinh444@gmail.com>
2018-11-07 16:57:17 -0500

    add jawDB connection and .env npm

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
+require('dotenv').config();
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: "root",
-    password: "password23P^",
-  database: "burgers_db"
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+
+  connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db"
 });
+
+}
 
 // Create connection.
 connection.connect(function(err) {
